096cec976d72b67d0c84f766696f5b8ba7991c51
Alex <alexrasla@ucsb.edu>
2022-02-20 20:21:00 -0800

    can ask more than one thing

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Oculus.Voice.Demo.UIShapesDemo;
+
+public class HandController : MonoBehaviour
+{
+
+    private InputDevice targetDevice;
+
+    private float time = 0f;
+    private bool active = false;
+    public GameObject interactionsHandler;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics =
+            InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float enableValue);
+        
+        if ((enableValue > 0.1f && !active) || Input.GetKeyDown(KeyCode.M))
+        {
+            interactionsHandler.GetComponent<InteractionHandler>().ToggleActivation();
+            active = true;
+        }
+
+        if (active && (Time.time - time > 5f))
+        {
+            active = false;
+        }
+
+    }
+}
